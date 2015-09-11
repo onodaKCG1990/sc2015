012df40be1e13f58c1b74606ee089b4a5afcf496
onodaKCG1990 <onoda1990ke@gmail.com>
2015-09-11 09:40:42 +0900

    <add> function multi

--- practice.js
+++ practice.js
@@ -2,6 +2,18 @@ Number.prototype.plus = function ( num ) {
   return this + num;
 };
 
+Number.prototype.multi = function ( num ) {
+  return this * num;
+};
+
+
+Number.prototype.division = function ( num ) {
+  return this / num;
+};
+
+
 var number = 8;
 var result = number.plus(100);
+var result2 = number.multi(100);
+var result3 = number.division(2);
 console.log(result);
